45aee48542cf6632130ad930b5b63c95d5163c06
Michael VanBemmel <michael.vanbemmel@gmail.com>
2022-12-23 16:48:17 -0800

    Create proc-macro2-nom.
    Use it instead of the outdated combine-proc-macro crate in mvbitfield. Also update a few straggler dependencies that were more obvious after all of this dependency cleanup.

--- shared/mvbitfield-proc-macro/src/ast.rs
+++ shared/mvbitfield-proc-macro/src/ast.rs
@@ -1,8 +1,11 @@
-use combine::combinator::recognize;
-use combine::{between, choice, optional, sep_end_by1, ParseError, Parser, Stream};
-use combine_proc_macro::parser::{delim, ident, keyword, literal, punct};
-use combine_proc_macro::Token;
-use proc_macro2::{Ident, Literal, TokenStream, TokenTree};
+use nom::combinator::{opt, recognize};
+use nom::multi::separated_list1;
+use nom::sequence::{terminated, tuple};
+use nom::{Finish, Parser};
+use proc_macro2::{Delimiter, Ident, Literal, TokenStream};
+use proc_macro2_nom::{
+    delim, descend, ident, keyword, literal, punct, FlatTokenStream, FlatTokenStreamSlice,
+};
 
 pub(crate) struct StructAst {
     pub header: StructAstHeader,
@@ -22,56 +25,142 @@ pub(crate) struct FieldAst {
     pub io_type: Option<Ident>,
 }
 
-pub(crate) fn struct_ast<I>() -> impl Parser<Input = I, Output = StructAst>
-where
-    I: Stream<Item = Token>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    struct Body {
-        fields: Vec<FieldAst>,
+fn visibility<'input>(
+) -> impl Parser<FlatTokenStreamSlice<'input>, TokenStream, proc_macro2_nom::Error> {
+    recognize(opt(tuple((
+        keyword("pub"),
+        opt(descend(delim(Delimiter::Parenthesis), keyword("crate"))),
+    ))))
+    .map(FlatTokenStreamSlice::to_token_stream)
+}
+
+fn header<'input>(
+) -> impl Parser<FlatTokenStreamSlice<'input>, StructAstHeader, proc_macro2_nom::Error> {
+    tuple((visibility(), keyword("struct"), ident, punct(':'), ident)).map(
+        |(visibility, _, name, _, repr)| StructAstHeader {
+            visibility,
+            name: name.clone(),
+            repr: repr.clone(),
+        },
+    )
+}
+
+fn field<'input>() -> impl Parser<FlatTokenStreamSlice<'input>, FieldAst, proc_macro2_nom::Error> {
+    tuple((
+        visibility(),
+        ident,
+        punct(':'),
+        literal,
+        opt(tuple((keyword("as"), ident)).map(|(_, io_type)| io_type)),
+    ))
+    .map(|(visibility, name, _, width, io_type)| FieldAst {
+        visibility,
+        name: name.clone(),
+        width: width.clone(),
+        io_type: io_type.cloned(),
+    })
+}
+
+fn struct_decl<'input>(
+) -> impl Parser<FlatTokenStreamSlice<'input>, StructAst, proc_macro2_nom::Error> {
+    tuple((
+        header(),
+        descend(
+            delim(Delimiter::Brace),
+            terminated(separated_list1(punct(','), field()), opt(punct(','))),
+        ),
+    ))
+    .map(|(header, fields)| StructAst { header, fields })
+}
+
+pub(crate) fn parse(input: TokenStream) -> StructAst {
+    let mut parser = struct_decl();
+    parser
+        .parse(FlatTokenStream::new(input).slice())
+        .finish()
+        .unwrap()
+        .1
+}
+
+#[cfg(test)]
+mod tests {
+    use quote::quote;
+
+    use super::*;
+
+    fn apply<'input, T>(
+        mut parser: impl Parser<FlatTokenStreamSlice<'input>, T, proc_macro2_nom::Error>,
+        input: &'input FlatTokenStream,
+    ) -> T {
+        parser.parse(input.slice()).finish().unwrap().1
     }
 
-    let visibility = || {
-        recognize::<Vec<_>, _>(optional(choice((
-            keyword("pub").map(drop),
-            (keyword("pub"), delim('('), keyword("crate"), delim(')')).map(drop),
-        ))))
-        .map(|vec| {
-            vec.into_iter()
-                .map(|x: Token| TokenTree::try_from(x).unwrap())
-                .collect()
-        })
-    };
+    #[test]
+    fn visibility_empty() {
+        let input = quote! {};
+        let parser = visibility();
+        let result = apply(parser, &FlatTokenStream::new(input));
+        assert_eq!(result.to_string(), "");
+    }
 
-    let header = (
-        visibility(),
-        keyword("struct"),
-        ident(),
-        punct(':'),
-        ident(),
-    )
-        .map(|(visibility, _, name, _, repr)| StructAstHeader {
-            visibility,
-            name,
-            repr,
-        });
+    #[test]
+    fn visibility_pub() {
+        let input = quote! { pub };
+        let parser = visibility();
+        let result = apply(parser, &FlatTokenStream::new(input.clone()));
+        assert_eq!(result.to_string(), input.to_string());
+    }
 
-    let field = (
-        visibility(),
-        ident(),
-        punct(':'),
-        literal(),
-        optional((keyword("as"), ident()).map(|(_, io_type)| io_type)),
-    )
-        .map(|(visibility, name, _, width, io_type)| FieldAst {
-            visibility,
-            name,
-            width,
-            io_type,
-        });
+    #[test]
+    fn visibility_pub_crate() {
+        let input = quote! { pub(crate) };
+        let parser = visibility();
+        let result = apply(parser, &FlatTokenStream::new(input.clone()));
+        assert_eq!(result.to_string(), input.to_string());
+    }
 
-    let body = between(delim('{'), delim('}'), sep_end_by1(field, punct(',')))
-        .map(|fields| Body { fields });
+    #[test]
+    fn header_default_visibility() {
+        let input = quote! { struct Foo: u32 };
+        let parser = header();
+        let result = apply(parser, &FlatTokenStream::new(input));
+        assert!(result.visibility.is_empty());
+        assert_eq!(result.name.to_string(), "Foo");
+        assert_eq!(result.repr.to_string(), "u32");
+    }
 
-    (header, body).map(|(header, Body { fields })| StructAst { header, fields })
+    #[test]
+    fn header_pub() {
+        let input = quote! { pub struct Foo: U24 };
+        let parser = header();
+        let result = apply(parser, &FlatTokenStream::new(input));
+        assert_eq!(result.visibility.to_string(), "pub");
+        assert_eq!(result.name.to_string(), "Foo");
+        assert_eq!(result.repr.to_string(), "U24");
+    }
+
+    #[test]
+    fn field_default() {
+        let input = quote! { my_field: 5 };
+        let parser = field();
+        let result = apply(parser, &FlatTokenStream::new(input));
+        assert_eq!(result.visibility.to_string(), "");
+        assert_eq!(result.name.to_string(), "my_field");
+        assert_eq!(result.width.to_string(), "5");
+        assert!(result.io_type.is_none());
+    }
+
+    #[test]
+    fn field_everything() {
+        let input = quote! { pub(crate) my_field: 5 as Bar };
+        let parser = field();
+        let result = apply(parser, &FlatTokenStream::new(input));
+        assert_eq!(
+            result.visibility.to_string(),
+            quote! { pub(crate) }.to_string(),
+        );
+        assert_eq!(result.name.to_string(), "my_field");
+        assert_eq!(result.width.to_string(), "5");
+        assert_eq!(result.io_type.unwrap().to_string(), "Bar");
+    }
 }
--- shared/mvbitfield-proc-macro/src/lib.rs
+++ shared/mvbitfield-proc-macro/src/lib.rs
@@ -1,7 +1,4 @@
-use combine::Parser;
-use combine_proc_macro::{Incomplete, Input};
-
-use crate::ast::struct_ast;
+use crate::ast::parse;
 use crate::decl::StructDecl;
 
 mod ast;
@@ -10,24 +7,16 @@ mod types;
 
 #[proc_macro]
 pub fn mvbitfield(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let (ast, trailing) = struct_ast()
-        .easy_parse(Input::from(input).with_lookahead(1))
-        .map_err(|e| panic!("parse error: {e:#?}"))
-        .unwrap();
-    if let Some(e) = Incomplete::from_stream(trailing) {
-        panic!("Unexpected trailing input:\n{e}");
-    }
-
-    StructDecl::from_ast(ast).into_token_stream().into()
+    StructDecl::from_ast(parse(input.into()))
+        .into_token_stream()
+        .into()
 }
 
 #[cfg(test)]
 mod tests {
-    use combine::Parser;
-    use combine_proc_macro::{Incomplete, Input};
     use quote::quote;
 
-    use crate::ast::struct_ast;
+    use crate::ast::parse;
     use crate::decl::StructDecl;
 
     #[test]
@@ -39,12 +28,7 @@ mod tests {
             }
         };
 
-        let (ast, trailing) = struct_ast()
-            .easy_parse(Input::from(input).with_lookahead(1))
-            .map_err(|e| panic!("parse error: {e:#?}"))
-            .unwrap();
-        assert!(Incomplete::from_stream(trailing).is_none());
-
+        let ast = parse(input);
         let _ = StructDecl::from_ast(ast).into_token_stream();
     }
 }
